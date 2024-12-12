8ad81762a668fc45d145cd3db9606748c9d60e37
Kensufox <pokenfox@gmail.com>
2024-12-12 19:03:13 +0100

    Adding shape plugin
    Adding the plugin I'm working on to make more shape for MinGL2 (and also try to make the oval work)

---
+++ Shape plugin for MinGL2 in progress/try_shape/main.cpp
@@ -0,0 +1,98 @@
+#define FPS_LIMIT 60
+
+#include <iostream>
+#include <thread>
+#include <cmath>
+
+#include "mingl/mingl.h"
+
+#include "mingl/shape/rectangle.h"
+#include "mingl/shape/circle.h"
+#include "mingl/shape/line.h"
+#include "mingl/shape/triangle.h"
+#include <tuple>
+
+using namespace std;
+
+// void player_sprite(MinGL &window, tuple<int,int> window_size, tuple<int,int> pos){
+//     //define the player sprite position
+//     tuple<int, int> size = (window_size[0]/2,window_size[1]/2);
+//     window << nsShape::Circle(nsGraphics::Vec2D(size), 10, nsGraphics::KYellow);
+// }
+
+void draw_oval(MinGL &window, tuple <int, int> pos, tuple <int, int> size, int resolution){
+    float degre = 2 * M_PI / resolution;
+    for (int i=0; i < resolution; ++i){
+        //calcul des positions des différents points avec sinus
+        float TriP1_1 = (get<0>(size)*(cos(degre*(i+0))))+300;
+        float TriP1_2 = (get<1>(size)*(sin(degre*(i+0))))+300;
+        if (i == resolution-1){i = 0;}
+        float TriP2_1 = get<0>(pos)+(get<1>(size)/2);
+        float TriP2_2 = get<1>(pos)+(get<1>(size)/2);
+        float TriP3_1 = get<0>(size)*(cos(degre*(i+1)))+300;
+        float TriP3_2 = get<1>(size)*(sin(degre*(i+1)))+300;
+        window << nsShape::Triangle(nsGraphics::Vec2D(TriP1_1,TriP1_2),
+                                    nsGraphics::Vec2D(TriP2_1,TriP2_2),
+                                    nsGraphics::Vec2D(TriP3_1,TriP3_2),
+                                    nsGraphics::KBlue
+                                    );
+    }
+}
+
+void dessiner(MinGL &window, float &mouth, int move)
+{
+    // int TriP1_1 = 320 + move;
+    // int TriP2_1 = 600 + move;
+    // int TriP3_1 = 600 + move;
+    // int CirP1 = 320 + move;
+    // float TriP2_2 = 420 - (100 * mouth);
+    // float TriP3_2 = 220 + (100 * mouth);
+    // window << nsShape::Circle(nsGraphics::Vec2D(CirP1, 320), 10, nsGraphics::KYellow);
+    // window << nsShape::Triangle(nsGraphics::Vec2D(TriP1_1, 320), nsGraphics::Vec2D(TriP2_1, TriP2_2), nsGraphics::Vec2D(TriP3_1, TriP3_2), nsGraphics::KBlack);
+    draw_oval(window, tuple <int, int>(300,300), tuple <int, int>(100,200), 100);
+
+}
+
+int main()
+{
+    // Initialise le système
+    MinGL window("01 - Shapes", nsGraphics::Vec2D(640, 640), nsGraphics::Vec2D(128, 128), nsGraphics::KBlack);
+    window.initGlut();
+    window.initGraphic();
+    int val = 0;
+    float mouth;
+    int move = 0;
+
+    // Variable qui tient le temps de frame
+    chrono::microseconds frameTime = chrono::microseconds::zero();
+
+    while (window.isOpen())
+    {
+        mouth = sin(val);
+        val++;
+        move = move + 1;
+
+        // Récupère l'heure actuelle
+        chrono::time_point<chrono::steady_clock> start = chrono::steady_clock::now();
+
+        // On efface la fenêtre
+        window.clearScreen();
+
+        // On dessine les formes géométriques
+        dessiner(window, mouth, move);
+
+        // On finit la frame en cours
+        window.finishFrame();
+
+        // On vide la queue d'évènements
+        window.getEventManager().clearEvents();
+
+        // On attend un peu pour limiter le framerate et soulager le CPU
+        this_thread::sleep_for(chrono::milliseconds(2500 / FPS_LIMIT) - chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start));
+
+        // On récupère le temps de frame
+        frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
+    }
+
+    return 0;
+}
