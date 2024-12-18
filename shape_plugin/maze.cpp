ce3bbb3693b12646e077400fb284b2f02debf7fb
Kensufox <matisse.quilliec@etu.univ-amu.fr>
2024-12-18 09:07:07 +0100

    Make the sprite game size proportionnal

--- shape_plugin/maze.cpp
+++ shape_plugin/maze.cpp
@@ -24,7 +24,7 @@ typedef vector <CVLine> CMatrix; // un type représentant la grille
 typedef pair   <unsigned, unsigned> CPosition; // un type représentant une coordonnée dans la grille
 
 //défini les éléments du sprite du joueur 2
-void Player1Sprite(MinGL &window, int grid_x, int grid_y, int pos_x, int pos_y){
+void Player1Sprite(MinGL &window, int grid_x, int grid_y, int pos_x, int pos_y, int mat_x, int mat_y){
     //calcule la position de la tuile
     int Point1_1 = (grid_x*pos_x);
     int Point1_2 = (grid_x*pos_y);
@@ -33,18 +33,29 @@ void Player1Sprite(MinGL &window, int grid_x, int grid_y, int pos_x, int pos_y){
     window << nsShape::Rectangle(nsGraphics::Vec2D(Point1_1,Point1_2),
                                  nsGraphics::Vec2D(Point2_1,Point2_2),
                                  nsGraphics::KYellow);
-    //calcul de la position du personnage
+    //calcul de la position centrale du personnage
     int Circle1 = Point1_1+((Point2_1-Point1_1)/2);
     int Circle2 = Point1_2+((Point2_2-Point1_2)/2);
-    //shape formant le sprite
-    window << nsShape::Oval(nsGraphics::Vec2D(Point1_1+20,Point1_2+25), nsGraphics::Vec2D(13,8), nsGraphics::KMaroon);// panard droit
-    window << nsShape::Oval(nsGraphics::Vec2D(Point1_1+20,Point1_2+42), nsGraphics::Vec2D(13,8), nsGraphics::KMaroon);// panard gauche
-    window << nsShape::Circle(nsGraphics::Vec2D(Circle1, Circle2-17), 9, nsGraphics::KMaroon);  //epaule droite
-    window << nsShape::Circle(nsGraphics::Vec2D(Circle1, Circle2+17), 9, nsGraphics::KMaroon);  //epaule gauche
-    window << nsShape::Circle(nsGraphics::Vec2D(Circle1, Circle2), 20, nsGraphics::KBlue);    //contenant de gras
-    window << nsShape::Circle(nsGraphics::Vec2D(Circle1-10, Circle2-5), 2, nsGraphics::KBlack);   //oeil droit
-    window << nsShape::Circle(nsGraphics::Vec2D(Circle1-10, Circle2+5), 2, nsGraphics::KBlack);   //oeil gauche
+    //calcul variable de position et taille des formes
+    int foot_x = 110/mat_x;
+    int foot_y = 80/mat_y;
+    int foot_x_size = 100/mat_x;
+    int foot_y_size = 70/mat_y;
+    int should_y = 170/mat_y;
+    int should_size = 90/mat_x;
+    int body_size = 180/mat_x;
+    int eye_x = 130/mat_x;
+    int eye_y = 50/mat_y;
+    int eye_size = 20/mat_x;
 
+    //shape formant le sprite
+    window << nsShape::Oval(nsGraphics::Vec2D(Circle1-foot_x,Circle2-foot_y), nsGraphics::Vec2D(foot_x_size,foot_y_size), nsGraphics::KMaroon);// panard droit
+    window << nsShape::Oval(nsGraphics::Vec2D(Circle1-foot_x,Circle2+foot_y), nsGraphics::Vec2D((100/mat_x),(70/mat_y)), nsGraphics::KMaroon);// panard gauche
+    window << nsShape::Circle(nsGraphics::Vec2D(Circle1, Circle2-should_y), should_size, nsGraphics::KMaroon);  //epaule droite
+    window << nsShape::Circle(nsGraphics::Vec2D(Circle1, Circle2+should_y), should_size, nsGraphics::KMaroon);  //epaule gauche
+    window << nsShape::Circle(nsGraphics::Vec2D(Circle1, Circle2), body_size, nsGraphics::KBlue);    //contenant de gras
+    window << nsShape::Circle(nsGraphics::Vec2D(Circle1-eye_x, Circle2-eye_y), eye_size, nsGraphics::KBlack);   //oeil droit
+    window << nsShape::Circle(nsGraphics::Vec2D(Circle1-eye_x, Circle2+eye_y), eye_size, nsGraphics::KBlack);   //oeil gauche
 }
 
 //défini les éléments du sprite du joueur 1
@@ -102,7 +113,7 @@ void DisplayGrid (MinGL &window, const CMatrix & mat, tuple <int, int> Screen_si
         for (int j=0; j < mat_y; ++j){
             switch (mat[i][j]){
             case 'X':
-                Player1Sprite(window, grid_x, grid_y, i, j);
+                Player1Sprite(window, grid_x, grid_y, i, j, mat_x, mat_y);
                 break;
             case 'O':
                 Player2Sprite(window, grid_x, grid_y, i, j);
@@ -129,9 +140,9 @@ int main()
     CPosition size (10,10);
     //CMatrix mat (size.first, CVLine (size.second, '1'));
     CMatrix mat (size.first, CVLine (size.second, '_'));
-    mat[3][4] = 'X';
-    mat[2][3] = 'O';
-    mat[1][0] = 'M';
+    mat[0][0] = 'X';
+    // mat[2][3] = 'O';
+    // mat[1][0] = 'M';
 
     // Variable qui tient le temps de frame
     chrono::microseconds frameTime = chrono::microseconds::zero();
