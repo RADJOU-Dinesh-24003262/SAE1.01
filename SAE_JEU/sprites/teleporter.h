4e2acb53209f2f787e676b106fc85a35fe6b494e
q24019562 <163044607+Kensufox@users.noreply.github.com>
2025-01-05 20:03:13 +0000

    minor update

--- SAE_JEU/sprites/teleporter.h
+++ SAE_JEU/sprites/teleporter.h
@@ -22,6 +22,7 @@ void Teleporter(MinGL &window, int Circle1, int Circle2, int mat_x, int mat_y){
     int wall_x = 320/mat_x;
     int wall_y = 320/mat_y;
 
+
     //shape formant le sprite
     window << nsShape::Rectangle(nsGraphics::Vec2D((Circle1-wall_x),(Circle2-wall_y)), nsGraphics::Vec2D((Circle1+wall_x),(Circle2+wall_y)), nsGraphics::KPurple);// square
 }
