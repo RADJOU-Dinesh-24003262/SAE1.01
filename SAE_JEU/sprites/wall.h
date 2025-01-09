0fb9b855b740ca0f7dd31fffe890494de02595ac
q24019562 <163044607+Kensufox@users.noreply.github.com>
2025-01-09 21:48:35 +0000

    modif sprite

--- SAE_JEU/sprites/wall.h
+++ SAE_JEU/sprites/wall.h
@@ -10,10 +10,6 @@
 
 //include les différentes formes pour dessiner les sprites
 #include "mingl/shape/rectangle.h"
-// #include "mingl/shape/circle.h"
-// #include "mingl/shape/line.h"
-// #include "mingl/shape/triangle.h"
-// #include "mingl/shape/oval.h"
 
 //défini les éléments du sprite d'un mur
 void Wall(MinGL &window, int Circle1, int Circle2, int mat_x, int mat_y){
